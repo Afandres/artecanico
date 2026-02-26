fb61011ad5f74e5d57677e29211c56a8c2062d98
Anonimus23Fire <anonimus23fire@gmail.com>
2026-02-26 12:41:44 -0500

    Creacion del proyecto y autenticacion

---
+++ app.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('apirest');
+
+  items: string[] = [];
+
+  agregarItem() {
+    const nuevo = `Elemento ${this.items.length + 1}`;
+    this.items.push(nuevo);
+  }
+}
